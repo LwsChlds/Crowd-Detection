94afddf1616315399c41ac56bd87afb24c895936
LwsChlds <85299007+LwsChlds@users.noreply.github.com>
2021-07-14 16:07:09 +0100

    Updated to include the smaller model

--- src/config.py
+++ src/config.py
@@ -12,13 +12,22 @@ __C.VAL_BATCH_SIZE = 6
 __C.TEST_BATCH_SIZE = 6
 __C.N_WORKERS = 2
 
-__C.PRE_TRAINED = '05-27_18-12_VisDrone_MobileCount_0.0001__1080x1920/all_ep_16_mae_9.8_rmse_12.8.pth'
+# a 540x960 model to be used when not enough GPU memory is available to produce an onnx file of 1080x1920
+__C.PRE_TRAINED = '06-02_20-15_VisDrone_MobileCountx0_5_0.0001__540x960_GC/all_ep_43_mae_16.4_rmse_21.3.pth'
+
+# use this when wanting to use the larger more accurate model
+#__C.PRE_TRAINED = '05-27_18-12_VisDrone_MobileCount_0.0001__1080x1920/all_ep_16_mae_9.8_rmse_12.8.pth'
 
 # path settings
 __C.EXP_PATH = '../exp'
 __C.DATASET = 'VisDrone'
-__C.NET = 'MobileCount'
-__C.DETAILS = '_1080x1920_NVS'
+# config settings for the smaller model
+__C.NET = 'MobileCountx0_5'
+__C.DETAILS = '_540x960_GC'
+
+# use when wanting to use the larger more accurate model
+#__C.NET = 'MobileCount'
+#__C.DETAILS = '_1080x1920_NVS'
 
 # learning optimizer settings
 __C.LR = 1e-4  # learning rate
