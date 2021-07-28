5dcce8b61254c741f09b21fd832e1956b60c54ce
LwsChlds <85299007+LwsChlds@users.noreply.github.com>
2021-07-28 13:35:40 +0100

    Updated to include dbscan

--- onnxRuntime/inference.py
+++ onnxRuntime/inference.py
@@ -1,6 +1,6 @@
 import numpy as np
 import preprocessing as pre
-import postprocessing as post
+import dbscan as post
 from PIL import Image
 import onnxruntime as rt
 import json
@@ -21,11 +21,28 @@ else:
 if config.get('properties', 'onnx', fallback=0) != 0:
     onnx = str(config.get('properties', 'onnx'))
 else:
-    print("No onnx file was foudn in the spec file")
+    print("No onnx file was found in the spec file")
+
+Length = 960 # the amount of pixels the input/output length contains
+Height = 540 # the amount of pixels the input/output height contains
+epsilon = 20
+min_samples = 500
+
+# if the values are present in the config file read from file
+# if they are not present it uses the system default values instead
+if config.get('properties', 'Length', fallback=0) != 0:
+    Length = int(config.get('properties', 'Length'))
+if config.get('properties', 'Height', fallback=0) != 0:
+    Height = int(config.get('properties', 'Height'))
+if config.get('properties', 'epsilon', fallback=0) != 0:
+    epsilon = int(config.get('properties', 'epsilon'))
+if config.get('properties', 'min_samples', fallback=0) != 0:
+    min_samples = int(config.get('properties', 'min_samples'))
+
 print("Preprocessing data")
 
 # normalising the data before using onnxRuntime
-normalised = pre.preprocess(inputIMG, config)
+normalised = pre.preprocess(inputIMG, Height, Length)
 
 # running onnxRuntime
 data = json.dumps({'data': (normalised).tolist()})
@@ -36,4 +53,4 @@ input_name = session.get_inputs()[0].name
 output_name = session.get_outputs()[0].name
 result = session.run([output_name], {input_name: data})
 print("Postprocessing data")
-post.postprocess(np.array(result), outputIMG, config)
+post.postprocess(np.array(result)[0][0][0], epsilon=epsilon, min_samples=min_samples, outputIMG=outputIMG)
