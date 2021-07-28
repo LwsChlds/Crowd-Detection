541c29b8e42834e978592b54a83e0d8b65e1a192
LwsChlds <85299007+LwsChlds@users.noreply.github.com>
2021-07-28 13:36:07 +0100

    Updated to remove config file being read each call

--- onnxRuntime/preprocessing.py
+++ onnxRuntime/preprocessing.py
@@ -2,20 +2,10 @@ import numpy as np
 from PIL import Image
 import configparser
 
-def preprocess(inputIMG, config):
+def preprocess(inputIMG, Height, Length):
   
     MEAN = np.array([0.43476477, 0.44504763, 0.43252817])
     STD = np.array([0.20490805, 0.19712372, 0.20312176])
-
-    Length = 960 # the amount of pixels the input/output length contains
-    Height = 540 # the amount of pixels the input/output height contains
-
-    # if the values are present in the config file read from file
-    # if they are not present it uses the system default values instead
-    if config.get('properties', 'Length', fallback=0) != 0:
-        Length = int(config.get('properties', 'Length'))
-    if config.get('properties', 'Height', fallback=0) != 0:
-        Height = int(config.get('properties', 'Height'))
 
     ordered = np.full(shape=(3, Height, Length), fill_value=128.0)
     # resizing the image whilst keeping original proportions
