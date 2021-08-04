83a3361b2620018473b7802fc9545c1bbe6edcaf
LwsChlds <85299007+LwsChlds@users.noreply.github.com>
2021-08-04 12:43:42 +0100

    Added the ability to process from an array input

--- onnxRuntime/preprocessing.py
+++ onnxRuntime/preprocessing.py
@@ -7,9 +7,14 @@ def preprocess(inputIMG, Height, Length):
     STD = np.array([0.20490805, 0.19712372, 0.20312176])
 
     ordered = np.full(shape=(3, Height, Length), fill_value=128.0)
+
     # resizing the image whilst keeping original proportions
     basewidth = Length
-    img = Image.open(inputIMG)
+    #img = Image.open(inputIMG)
+    if str(type(inputIMG)) == "<class 'numpy.ndarray'>":
+        img = Image.fromarray(inputIMG)
+    else:
+        img = inputIMG
     wpercent = (basewidth/float(img.size[0]))
     hsize = int((float(img.size[1])*float(wpercent)))
     img = img.resize((basewidth,hsize), Image.ANTIALIAS)
@@ -18,7 +23,7 @@ def preprocess(inputIMG, Height, Length):
 
     # preprocessing data
     array = (((array + 3.75)/255) - MEAN[None, None, :]) / STD[None, None, :]
+
     ordered = np.moveaxis(array, -1, 0)
     ordered = ordered[np.newaxis, ...]
-
     return ordered
